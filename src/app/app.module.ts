6a25e4974c993e63e07c2065fbe87ab9408bed9c
alex w <103083020@student.swin.edu.au>
2020-10-15 21:02:26 +1100

    Quick set up  for tomorow

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiplayComponent } from './routers/diplay/diplay.component';
+import { PickyourweaponsComponent } from './routers/pickyourweapons/pickyourweapons.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiplayComponent,
+    PickyourweaponsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/routers/pickyourweapons/pickyourweapons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pickyourweapons',
+  templateUrl: './pickyourweapons.component.html',
+  styleUrls: ['./pickyourweapons.component.css']
+})
+export class PickyourweaponsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
