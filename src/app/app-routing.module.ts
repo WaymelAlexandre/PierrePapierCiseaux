f5ac4465a78b9cb8affd830b18b3d307535941f7
alex w <103083020@student.swin.edu.au>
2020-10-16 17:47:58 +1100

    Not sure what I doing (need to post and resquest the reslut now )

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DiplayComponent } from './routers/diplay/diplay.component';
+import { PickyourweaponsComponent } from './routers/pickyourweapons/pickyourweapons.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  { path: "weapon", component: PickyourweaponsComponent },
+
+  { path: " display ", component: DiplayComponent  },
+
+
+  { path: "**", redirectTo: "weapon"}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pcp.service.ts
+++ src/app/pcp.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PCPService {
 
-  constructor() { }
+  private playerSelection?: "Rock" | "Papper" | "Scissors" ;
+
+
+  constructor(private router : Router) { }
+
+  onSelection(option: "Rock" | "Papper" | "Scissors")
+  {
+    this.playerSelection = option;
+    this.router.navigateByUrl('/diplay')
+  }
+
 }
