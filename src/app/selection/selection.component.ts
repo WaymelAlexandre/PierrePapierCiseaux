a286f7cecedb4191094433d4ae28b5bd577bc63b
alex w <103083020@student.swin.edu.au>
2020-10-24 11:40:56 +1100

    color on select done + shoot button + alert if no choose as been made

--- src/app/selection/selection.component.ts
+++ src/app/selection/selection.component.ts
@@ -12,8 +12,31 @@ export class SelectionComponent implements OnInit {
   ngOnInit(): void {
   }
 
+
+  public RockPick = false ;
+  public PaperPick = false ;
+  public ScissorsPick = false ;
+
+
+
+
   selectOp(option: "Rock" | "Paper" | "Scissors") // selectOp  Button for the
   {
-    this.pierreFeuilleCiseauxService.commitSelection(option)
+    if (option == "Rock"){ this.RockPick = true;  this.PaperPick = false; this.ScissorsPick = false}
+    if (option == "Paper"){ this.RockPick = false;  this.PaperPick = true; this.ScissorsPick = false }
+    if (option == "Scissors"){ this.RockPick = false;  this.PaperPick = false; this.ScissorsPick = true }
   }
+
+  validationUser(){
+    if (this.RockPick == false && this.PaperPick == false && this.ScissorsPick == false) {
+      alert("You must pick your weapon to play ")
+    }
+    if ( this.RockPick){this.pierreFeuilleCiseauxService.commitSelection("Rock")}
+    if ( this.PaperPick){this.pierreFeuilleCiseauxService.commitSelection("Paper")}
+    if ( this.ScissorsPick){this.pierreFeuilleCiseauxService.commitSelection("Scissors")}
+
+  }
+
+
+
 }
